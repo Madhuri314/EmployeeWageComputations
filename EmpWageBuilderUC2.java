0314f6813546e8351553b3c6fe9becdd31ff6dca
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-27 02:31:17 +0530

    Calculated daily employee wages

---
+++ EmpWageBuilderUC2.java
@@ -0,0 +1,23 @@
+public class EmpWageBuilderUC2{
+        public static final int isPresent=1;
+        public static final int ratePerHour=20;
+
+        public static void main(String args[]){
+                int empHours=0;
+                int empWagesPerDay=0;
+
+                double randomCheck=Math.floor(Math.random()*10)%2;
+
+                if(randomCheck==isPresent){
+                        System.out.println("Employee is present");
+                        empHours=8;
+                }
+                else{
+                        System.out.println("Employee is absent");
+	      empHours=0;
+                }
+
+                empWagesPerDay=empHours*ratePerHour;
+                System.out.println("Employee Wage for a day: "+empWagesPerDay);
+        }
+}
